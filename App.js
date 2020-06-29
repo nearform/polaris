538f9154731c2296794f2aa86b46cf1d8995ca9d
Andrea Forni <andrea.forni@nearform.com>
2020-06-29 12:09:04 +0200

    feat: load i18n service in App.js and add translations to React components

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
-import * as React from 'react'
-import { View, Text, StatusBar, Button } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import * as React from "react";
+import { View, Text, StatusBar, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useTranslation } from "react-i18next";
+import "./services/i18n";
 
-const HomeScreen = ({ navigation }) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <StatusBar style="auto" />
-    <Text>Home Screen</Text>
-    <Button
-      title="Go to Details"
-      onPress={() => navigation.push('Details')}
-    />
-  </View>
-)
-const DetailsScreen = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <StatusBar style="auto" />
-    <Text>Details Screen</Text>
-  </View>
-)
+const HomeScreen = ({ navigation }) => {
+  const { t } = useTranslation();
 
-const Stack = createStackNavigator()
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <StatusBar style="auto" />
+      <Text>{t("home:title")}</Text>
+      <Button
+        title={t("home:button")}
+        onPress={() => navigation.push("Details")}
+      />
+    </View>
+  );
+};
+const DetailsScreen = () => {
+  const { t } = useTranslation();
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <StatusBar style="auto" />
+      <Text>{t("details:title")}</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
 
 const App = () => (
   <NavigationContainer>
@@ -29,6 +39,6 @@ const App = () => (
       <Stack.Screen name="Details" component={DetailsScreen} />
     </Stack.Navigator>
   </NavigationContainer>
-)
+);
 
-export default App
+export default App;
