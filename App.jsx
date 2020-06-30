db4fc11d43a575d74b7f32395af7712e5ac809e7
Sergey Kurdin <kurdin@gmail.com>
2020-06-30 11:44:27 -0400

    root App point to src/App

--- App.jsx
+++ App.jsx
@@ -1,19 +1,3 @@
-import React, { Component } from 'react';
-import { initialState, Provider, AppState } from './src/store';
-import { YellowBox } from 'react-native';
+import App from './src/App';
 
-import { UIProvider } from './src/store/ui/context';
-import App from './src';
-
-// Mute multiple known warnings to display on device. Those warning messages coming from React Native components and need to be fixed by RN team
-YellowBox.ignoreWarnings(['Animated', 'Warning: componentWill', 'Possible Unhandled Promise']);
-
-const Main = () => {
-	return (
-		<UIProvider>
-			<App />
-		</UIProvider>
-	);
-};
-
-export default Main;
+export default App;
