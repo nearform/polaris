7ef7caed50e6acc7de54d25e6985d509e34ba9d1
Sergey <skurdin@yahoo.com>
2020-06-30 12:00:02 -0400

    Update src/components/organisms/tab-bar/index.jsx
    Co-authored-by: Andrea Forni <1233582+andreaforni@users.noreply.github.com>

--- src/components/organisms/tab-bar/index.jsx
+++ src/components/organisms/tab-bar/index.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useEffect, useMemo } from 'react';
-import { View, StyleSheet, Animated, Text } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 
 const TabBar = () => {
   return (
