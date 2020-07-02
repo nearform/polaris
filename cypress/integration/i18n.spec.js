52c8c6b258bae08d968dea9393bf6d136c16319d
Andrea Forni <andrea.forni@nearform.com>
2020-07-02 17:31:30 +0200

    chore: better file name and updated test's description

--- cypress/integration/i18n.spec.js
+++ cypress/integration/i18n.spec.js
@@ -1,8 +1,8 @@
 import translationEN from '../../src/lang/en.json';
 import translationIT from '../../src/lang/it.json';
 
-describe('My First Test', () => {
-  it('Does not do much!', () => {
+describe('Internationalisation support', () => {
+  it('changes the app language', () => {
     cy.visit('http://localhost:19006/');
 
     cy.contains(translationEN.home.settingsButton).click();
