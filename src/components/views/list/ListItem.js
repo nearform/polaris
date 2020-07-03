d2d6f6bd26b8a9087c175c3dde66469fd0e1dd79
Alan Slater <alan.slater@nearform.com>
2020-07-03 14:33:46 +0100

    Routing / nav for native & web + cross-platform hooks (#32)
    * Install react-router-dom
    
    * Add 404 view
    
    * Add cross-platform support for params
    
    * Add cross-platform LinkButton
    
    * Add views showcasing params and query strings
    
    * Add central source of route definitions
    
    * Add platform-specific hooks for routing/location
    
    * Add and use routing template
    
    * Update home/index as JS file with LinkButtons
    
    * Update aliases after rebase
    
    * Use kebab-case in component dir for consistency
    
    * Regenerate package.lock after rebase
    
    * npm run lint:fix
    
    * Fix rebased-away missing context provider
    
    * Remove stray console.log()s
    
    * Add usePlatformParams to avoid circular dependency
    
    So that components in a route don't need to import something that imports routes
    
    * Add Link component, basing LinkButton on it
    
    * Add Links to nav, enabling routing in Layout
    
    * Use Drawer navigation with custom header
    
    * Update translations and use with route names
    
    * Improve style of picker and links
    
    * Fix lint
    
    * Document routing in readme
    
    * Remove commented out code
    
    * Integrate Camera into native-only menu
    
    Also uncovers and fixes small bug with native route ordering
    
    * Update package-lock.json
    
    * Fix label expected by e2e

---
+++ src/components/views/list/ListItem.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+import usePlatformParams from 'hooks/usePlatformParams';
+import usePlatformNavigation from 'hooks/usePlatformNavigation';
+
+import contentList from './content';
+
+export const ListItem = () => {
+  const { navigate } = usePlatformNavigation();
+  const params = usePlatformParams();
+  const contentItem = params && contentList.find(({ id }) => id === Number(params.id));
+
+  if (!contentItem) {
+    navigate('/listView');
+    return '';
+  }
+
+  const { title, content } = contentItem;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <StatusBar style="auto" />
+      <Text>{title}</Text>
+      <Text>{content}</Text>
+    </View>
+  );
+};
---
+++ src/hooks/usePlatformParams.native.js
@@ -0,0 +1,8 @@
+import { useRoute } from '@react-navigation/native';
+
+const usePlatformParams = () => {
+  const nativeRoute = useRoute();
+  return nativeRoute.params || {};
+};
+
+export default usePlatformParams;
