7cce3a965622efb32b36b8ecce41fead432b83a1
AlanSl <alan.slater@nearform.com>
2020-07-08 01:55:25 +0100

    Update tests to use real i18n and web picker

---
+++ src/components/molecules/language-selector/__tests__/index.test.web.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { act, render, fireEvent } from 'react-native-testing-library';
+import LanguageSelector from '../index';
+
+const LanguageTestContainer = () => {
+  const { i18n } = useTranslation();
+  return (
+    <View>
+      <LanguageSelector />
+      <Text>langcode: {i18n.language}</Text>
+    </View>
+  );
+};
+
+describe('Language Selector', () => {
+  it('language change updates i18n langcode', async () => {
+    const { getByTestId, queryAllByText } = render(<LanguageTestContainer />);
+    const languagePicker = getByTestId('language-selector');
+
+    await act(async () => {
+      await fireEvent(languagePicker, 'valueChange', 'en');
+    });
+    const en1 = queryAllByText('langcode: en');
+    const it1 = queryAllByText('langcode: it');
+    expect(en1).toHaveLength(1);
+    expect(it1).toHaveLength(0);
+
+    await act(async () => {
+      await fireEvent(languagePicker, 'valueChange', 'it');
+    });
+    const en2 = queryAllByText('langcode: en');
+    const it2 = queryAllByText('langcode: it');
+    expect(en2).toHaveLength(0);
+    expect(it2).toHaveLength(1);
+  });
+});
