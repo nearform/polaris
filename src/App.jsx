7477716d5d5347a459efb03ad1af695938f6f502
AlanSl <alan.slater@nearform.com>
2020-07-08 01:56:25 +0100

    Explicitly include i18n in app
    Previously it seemed to be included indirectly through subcomponents, which risks timing issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { YellowBox } from 'react-native';
 import Route from 'components/templates/route';
 import { UIProvider } from 'store/ui/context';
+import 'services/i18n'; // Attaches appropriate i18n object to react-i18n
 
 // Mute multiple known warnings to display on device. Those warning messages coming from React Native components and need to be fixed by RN team
 YellowBox.ignoreWarnings(['Animated', 'Warning: componentWill', 'Possible Unhandled Promise']);
