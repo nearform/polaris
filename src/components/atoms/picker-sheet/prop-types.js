c1f8b0593a351b444f7c7c50d237d3b28eee8e10
Alan Slater <alan.slater@nearform.com>
2020-07-08 15:08:57 +0100

    Cross-platform Picker component with real i18n in tests (#44)
    * Install @react-native-community/picker, run expo update
    
    Also add @react-native-community/picker to modules Jest must transpile
    
    * Split cross-platform picker and language-selector
    
    * Use cross-platform picker in ListView
    
    * Update tests to use real i18n and web picker
    
    * Explicitly include i18n in app
    
    Previously it seemed to be included indirectly through subcomponents, which risks timing issues
    
    * Update e2e test, using same test ID as unit tests
    
    * Add readme note on using i18n in tests
    
    * Write test name as "It {does thing}" sentence
    
    * Also write other test name as "It {does thing}"

---
+++ src/components/atoms/picker-sheet/prop-types.js
@@ -0,0 +1,16 @@
+import T from 'prop-types';
+
+export default {
+  onValueChange: T.func.isRequired,
+  options: T.arrayOf(
+    T.shape({
+      value: T.string.isRequired,
+      label: T.string // use value as label if label undefined
+    })
+  ),
+  style: T.object,
+  currentOption: T.object,
+  nativeID: T.string,
+  testID: T.string,
+  cancelLabel: T.string // Used in iOS only
+};
