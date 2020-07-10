764aa27f5b573d976281a69ec9f040dd98593c34
Andrea Forni <andrea.forni@nearform.com>
2020-07-10 15:48:33 +0200

    fix: bring back shared style for the container

--- src/components/views/home/index.jsx
+++ src/components/views/home/index.jsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { useTranslation } from 'react-i18next';
+import { styles } from '../views.styles';
 
 import LinkButton from 'components/molecules/link-button';
 
 export const HomeScreen = ({ navigation }) => {
   const { t } = useTranslation();
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <Text>{t('home:title')}</Text>
       <LinkButton title={t('home:viewOneButton')} path="/viewOne" />
       <LinkButton title={t('home:viewTwoButton')} path="/viewTwo" />
