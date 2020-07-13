66d47a3cfcaa0e5b33acccea65d2c3b76c2b9005
Andrea Forni <andrea.forni@nearform.com>
2020-07-13 10:49:36 +0200

    fix: bring back shared style for the container, remove unused prop.

--- src/components/views/home/index.jsx
+++ src/components/views/home/index.jsx
@@ -3,11 +3,12 @@ import { View, Text } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import NearformLogo from 'assets/logos/nearform.svg';
 import LinkButton from 'components/molecules/link-button';
+import { styles } from '../views.styles';
 
-export const HomeScreen = ({ navigation }) => {
+export const HomeScreen = () => {
   const { t } = useTranslation();
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <NearformLogo width={200} height={50} fill="#2165e3" title="Nearform logo" />
       <Text>{t('home:title')}</Text>
       <LinkButton title={t('home:viewOneButton')} path="/viewOne" />
