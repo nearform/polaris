822543019188def1bc1aea28be23b578af557638
Sergey Kurdin <kurdin@gmail.com>
2020-07-15 10:12:02 -0400

    Fix for storybook storyLoader.js not found, generate this file and add this to git

---
+++ storybook/storyLoader.js
@@ -0,0 +1,21 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/atoms/button/stories/button.stories.common');
+  require('../src/components/atoms/button/stories/button.stories.native');
+  require('../src/components/organisms/navigation/stories/nav.stories.native');
+}
+
+const stories = [
+  '../src/components/atoms/button/stories/button.stories.common',
+  '../src/components/atoms/button/stories/button.stories.native',
+  '../src/components/organisms/navigation/stories/nav.stories.native'
+];
+
+module.exports = {
+  loadStories,
+  stories
+};
