b2f4935be36a80e87dda64515154d16a4eafff89
Katie Roberts <katie.roberts@nearform.com>
2020-11-26 17:10:08 +0000

    stashing

--- src/components/views/bluetoothReader/index.web.jsx
+++ src/components/views/bluetoothReader/index.web.jsx
@@ -6,31 +6,6 @@ import Container from 'components/atoms/container'
 export const Bluetooth = () => {
   const { t } = useTranslation()
 
-  // const btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
-
-  /* btSerial.on('found', function(address, name) {
-    btSerial.findSerialPortChannel(address, function (channel) {
-      console.log('connected')
-
-      btSerial.write(Buffer.from('my data', 'utf-8'), function(err, bytesWritten) {
-        if (err) console.log(err);
-      });
-
-      btSerial.on('data', function(buffer) {
-        console.log(buffer.toString('utf-8'));
-      });
-    }, function () {
-    console.log('cannot connect');
-  });
-
-    // close the connection when you're ready
-    btSerial.close();
-    }, function() {
-    console.log('found nothing');
-
-})
-    btSerial.inquire(); */
-
   return (
     <Container>
       <Button
