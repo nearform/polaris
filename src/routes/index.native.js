4e58e923106a1bd7aa050432475443c298cc3be7
matijagaspar <matija.gaspar@gmail.com>
2020-11-30 16:15:50 +0100

    feat: map example

--- src/routes/index.native.js
+++ src/routes/index.native.js
@@ -1,5 +1,6 @@
 import { Camera } from 'components/views'
 import { QRCode } from 'components/views/QRCode'
+import { MapView } from 'components/views/MapView'
 
 import routes, { defaultPath, routeShape } from './index.common'
 
@@ -10,6 +11,12 @@ const nativeOnlyRoutes = [
     View: QRCode,
     name: 'qrCode:title',
     menuIndex: 11
+  },
+  {
+    path: '/map',
+    View: MapView,
+    name: 'map:title',
+    menuIndex: 12
   }
 ]
 
